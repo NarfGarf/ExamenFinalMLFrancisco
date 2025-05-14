ef5eecc8fb1f0279573db013927b44a8dbafea3a
Narf <Vidal.Carregal.Francisco@gmail.com>
2025-05-14 18:17:27 +0200

    Funciona Login

--- IniciarSesion.php
+++ IniciarSesion.php
@@ -14,35 +14,31 @@ $pass = "";
 $name = htmlspecialchars($_REQUEST["name"]);
 $pass = htmlspecialchars($_REQUEST["pass"]);
 
-login($name,$pass);
+
 
 //usar PreparedStatement para consultas sql para que no sql injeccion
 //$name == 'admin'&& $pass == 'pass'
-if ($name == 'admin'&& $pass == 'pass'){
+if (login($name,$pass)){
 ?>
     <h1>VALIDO</h1>
 <?php
 } else{
 ?>
     <h1>NO VALIDO</h1>
+    
 <?php
 }
 function login($name,$pass){
-    $sqlName = "SELECT nombre FROM usuarios";
-    echo($sqlName);
+    $sqlName = "SELECT * FROM usuarios";
     $dConnect = new  DatabaseConection;
     $datos = $dConnect-> exec_query($sqlName);
 
-    $arrayDatos = array();
-    while($row = mysqli_fetch_array($datos)){
-        $arrayDatos[] = $row;
+    while($row = mysqli_fetch_assoc($datos)){
+       if($name == $row["nombre"] && $pass == $row["pass"]){
+            return true;
+       }
     }
-    foreach($arrayDatos as &$e){
-        if($e == $name){
-            echo("USERNAME ENCONTRADO");
-        }
-        echo implode(" ",$e);
-    }
+    return false;
 }
 
 

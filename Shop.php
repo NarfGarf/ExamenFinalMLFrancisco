a820e3dfad5da7051cc1a24b5e300f2985603bbe
Narf <Vidal.Carregal.Francisco@gmail.com>
2025-05-17 10:40:04 +0200

    Creado el CSS de la pagina

--- Shop.php
+++ Shop.php
@@ -2,12 +2,18 @@
     <head>
         <title>Shop Fran</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+        <link rel="stylesheet" href="css.css">
         <?php 
             include "DatabaseConection.php";
         ?>
     
     </head>
     <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <a class="navbar-brand" >Shop de Palos y Rocas</a>
+    </nav>
+
+
         <div class="container">
         <div class="row">
         
@@ -17,22 +23,20 @@
         $datos = $dConnect-> exec_query($sqlName);
         while($row = mysqli_fetch_assoc($datos)){// automaticamente agrega los productos de la tabla a la pagina web
         ?>
-        <div class="card" style="width: 18rem;">
-            <img src="<?php echo $row["image"]?>" class="card-img-top" alt="...">
+        <div class="card">
+            <img src="<?php echo $row["image"]?>" class="card-img-top" alt="img">
             <div class="card-body">
                 <h5 class="card-title"><?php echo $row["nombre"]?></h5>
                 <p class="card-text"><?php echo $row["descripcion"]?></p>
-                <h5 class="card-title"><?php echo "$"; echo $row["precio"]?></h5>
-                <a href="#" class="btn btn-primary">Comprar</a>
+                <h5 class="card-money"><?php echo "$"; echo $row["precio"]?></h5>
+                <a class="btn btn-primary" >Comprar</a>
             </div>
         </div>
         <?php } ?>
         
         </div>
         </div>
-
-
-
+        <button type="button" class="btn btn-primary" onclick="location.href='index.html'">Logout</button>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </body>
 </html>
